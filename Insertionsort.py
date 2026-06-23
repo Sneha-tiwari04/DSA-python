5306f2656ba8cd018b60bf01e66ee4a31d93b55d
kiran dwivedi <snehatiwari7654321@gmail.com>
2026-06-23 16:43:18 +0530

    file

--- Insertionsort.py
+++ Insertionsort.py
@@ -1,12 +1,14 @@
 # onlineshorting 
-arr = [4,5,6,7,1,2,3]
+arr = [1,2,3,4,5]
+c=0
 
 def insertionSort(nums):
+    global c 
     n = len(nums)
-
     for i in range(1, n):
         j = i
         while j > 0 and nums[j] < nums[j-1]:
+            c=+1
             t = nums[j]
             nums[j] = nums[j-1]
             nums[j-1] = t
@@ -15,3 +17,4 @@ def insertionSort(nums):
     return nums
 
 print(insertionSort(arr))
+print("total hits=",c)
---
+++ selectionsort.py
@@ -0,0 +1,22 @@
+arr = [9,8,7,6,5]
+c=0
+def selection(nums):
+    n = len(nums)
+    global c
+    for i in range(n-1):
+        chhotu = i
+
+        for j in range(i+1, n):
+            c=+1
+            if nums[chhotu] > nums[j]:
+                chhotu = j
+
+        if chhotu != i:
+            t = nums[chhotu]
+            nums[chhotu] = nums[i]
+            nums[i] = t
+
+    return nums
+
+print(selection(arr))
+print("total hits=",c)
